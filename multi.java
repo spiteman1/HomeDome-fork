3555479726244d0310203bc995904f3a984dba79
Donell Zhanje <donelz2005@gmail.com>
2025-10-21 17:20:31 +0100

    Create multi.java

---
+++ multi.java
@@ -0,0 +1,15 @@
+public class multi{
+    int numA;
+    int numB;
+    public multi(int numa, int numb){
+        numA=numa;
+        numB=numb;
+
+
+    }
+    public int multi(int numa, int numb){
+
+        return numa*numb ;
+    }
+
+}
