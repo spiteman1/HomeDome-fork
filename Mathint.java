772e6c7e830b41f436cc277c24957c69fa1dcf24
amanjit805 <amanjit805@gmail.com>
2025-10-21 17:19:59 +0100

    Create Mathint.java

---
+++ Mathint.java
@@ -0,0 +1,8 @@
+public class Mathint{
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 36;
+        int result = a - b;
+        System.out.println(a +" - " + b +" = " + result);
+    }
+}
