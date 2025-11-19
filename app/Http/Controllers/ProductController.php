ea056d45286584ea4536f4e0da9bbc344174218d
hoonathan521-commits <hoonathan521@gmail.com>
2025-11-19 14:32:24 +0000

    Added class ProductController with a function show which has commented lines of code.

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,12 @@
+<?php 
+ namespace App\Http\Controllers; 
+
+ class ProductController extends Controller{
+
+    public function show($pid){
+        //Un-comment the following lines when needed and remove this comment 
+        //$product = Product::where('product_id', $pid)->first();
+        //return view('/show', array('product'=>$product))
+    }
+ }
+
