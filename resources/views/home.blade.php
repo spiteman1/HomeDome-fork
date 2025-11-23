4a9d133ccb8b6b4cc226fd8d4da0d706d6bc2968
Donell Zhanje <donelz2005@gmail.com>
2025-11-23 00:03:54 +0000

    Revert "made a home page"
    This reverts commit c67ca9903e23b4e55266a8f1a6c4723724fd8407.

--- resources/views/home.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.app')
-
-@section('title', 'HomeDome home')
-
-
--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
-});
-
-Route::get('p.search', function () {
-    return view('home');
+    return view('welcome');
 });
