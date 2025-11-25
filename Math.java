2048d870ff3ec9120d48447f9595404cead819b2
amanjit805 <amanjit805@gmail.com>
2025-11-25 12:36:18 +0000

    undo math,java

--- Math.java
+++
@@ -1,8 +0,0 @@
-public class Math{
-    public static void main(String[] args) {
-        int a = 100;
-        int b = 36;
-        int result = a - b;
-        System.out.println(a +" - " + b +" = " + result);
-    }
-}
