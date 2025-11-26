73a42ce77cecbada745c1f108c03f3a0f62369c8
hoonathan521-commits <hoonathan521@gmail.com>
2025-11-26 10:12:29 +0000

    Changed ProductController to account for product media with joins. Modified product.blade.php for the Database elements.

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,7 +4,13 @@
  class ProductController extends Controller{
 
     public function show($pid){
-        $product = Products::where('product_id', $pid)->first();
+        //Comment this line in case the join select query fails
+        //$product = Products::where('product_id', $pid)->first();
+        $product = DB::table('products')
+            ->join('product_media', 'products.product_id', '=', 'product_media.product_id')
+            ->select('products.*', 'product_media.*')
+            ->where('product_id', $pid)
+            ->first(); 
         return view('/show', array('product'=>$product));
     }
  }
