58a94b38d341b7cc514cea7143fccf24dbac1cbd
Tazdik Wazi Orshan <116716047+tazdikorshan@users.noreply.github.com>
2025-11-29 20:59:59 +0000

    Basic user authentication

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,50 @@
+@vite('resources/css/app.css')
+
+<header>
+    <div class="headerContainer">
+        <a class="headerLinks" href="/">
+            <img class="removeWhitespace" src="/images/homeDomeLogo.png" alt="homeDomeLogo"
+                height=60 width=60>
+            <h3 class="removeWhitespace">HomeDome</h3>
+        </a>
+
+        <form id="searchBar" action="" method="GET">
+            <input type="text" name="query" placeholder="Search products..." required>
+            <button type="submit">Search</button>
+        </form>
+
+        <div class="user-auth-section">
+            @auth
+            <p>Hello, {{ Auth::user()->name }}</p>
+
+            {{-- Logout --}}
+            <form action="{{ route('logout') }}" method="POST" class="logout-form">
+                @csrf
+                <button type="submit" class="headerLinks logout-btn">Logout</button>
+            </form>
+            @endauth
+
+            @guest
+            <p>Hello, <a class="headerLinks" href="{{ route('login') }}">Sign In or Sign Up</a></p>
+            @endguest
+        </div>
+
+
+        <a href="/">
+            <img src="/images/shopping_cart_icon.png" alt="shopping basket" height=50 width=50>
+        </a>
+
+    </div>
+    <section id="categories">
+
+
+        <a class="headerLinks" href="/furniture">Furniture</a>
+        <a class="headerLinks" href="/appliances">Appliances</a>
+        <a class="headerLinks" href="/homeDecor">Home Decor</a>
+        <a class="headerLinks" href="/kitchenWare">Kitchen Ware</a>
+        <a class="headerLinks" href="/lighting">Lighting</a>
+        <a class="headerLinks" href="/aboutUs">About Us</a>
+        <a class="headerLinks" href="/ourProducts">Our Products</a>
+
+    </section>
+</header>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+
+
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login'); // showing the login
+
+Route::post('/login', [AuthController::class, 'login'])->name('login-submit'); // form submission
+
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout'); // logout
+
+Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
+
+Route::post('/register', [AuthController::class, 'register'])->name('register-submit');
