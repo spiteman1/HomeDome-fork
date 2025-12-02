69935fe72dd57bab5928f4440bdeb8fe50cffda8
hoonathan521-commits <hoonathan521@gmail.com>
2025-12-02 13:29:21 +0000

    Adjusted the routing for show method to product/id instead of show/id to match the product.blade.php view.

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('show/{rid}', [ProductController::class, 'show']);
+Route::get('product/{rid}', [ProductController::class, 'show']);
 Route::get('/basket', [BasketController::class, 'listProducts'])->name('listBasketProducts'); 
 Route::post('updateQuantity/{bid}', [BasketController::class, 'updateQuantity'])->name('updateQuantity.updateQuantity'); 
 Route::post('addProduct/{pid}', [BasketController::class, 'addProduct'])->name('addProduct.addProduct'); 
