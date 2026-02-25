55de117f56de120649bdb2e5ae1953e01511c2c8
Donell Zhanje <donelz2005@gmail.com>
2026-02-25 10:47:30 +0000

    testing pulls

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -522,6 +522,59 @@ details > summary {
   cursor: pointer;
   color:white;
 }
+html, body {
+    height: 100%;
+    margin: 0;
+}
+
+body {
+    min-height: 100vh;
+    display: grid;
+    grid-template-rows: auto;
+}
+
+footer {
+    background: brown;
+    color: white;
+    padding: 25px 20px;
+}
+
+footer h4 {
+    font-size: 12px;
+    text-align: center;
+    margin-bottom: 15px;
+}
+
+.footer-box {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    max-width: 1000px;
+    margin: auto;
+    gap: 20px;
+}
+
+.footer-col {
+    flex: 1;
+    min-width: 150px;
+}
+
+.footer-col h5 {
+    margin-bottom: 10px;
+    font-size: 15px;
+}
+
+.footer-col a {
+    display: block;
+    color: white;
+    font-size: 15px;
+    text-decoration: none;
+    margin-bottom: 6px;
+}
+
+.footer-col a:hover {
+    text-decoration: underline;
+}
 </style>
 
     <div class="page">
@@ -734,5 +787,6 @@ details > summary {
             <a href="{{ route('category.show', 'lighting') }}">Lighting</a>
             <a class="headerLinks" href="{{route('About-Us')}}">About Us</a>
             <a href="{{route('Contact-us')}}"> Contact Us </a>
+            
 
         </div>
